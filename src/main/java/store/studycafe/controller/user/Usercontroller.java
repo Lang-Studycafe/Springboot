4043755c1b19e3316329e79d5657925cb5320ad9
hye-inA <hye.ina0130@gmail.com>
2024-10-05 15:10:33 +0900

    사용자 정보(핸드폰번호) 수정, 삭제 API 구현

--- src/main/java/store/studycafe/controller/user/Usercontroller.java
+++ src/main/java/store/studycafe/controller/user/Usercontroller.java
@@ -3,18 +3,15 @@ package store.studycafe.controller.user;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import store.studycafe.domain.User;
 import store.studycafe.dto.request.UserCreateRequest;
+import store.studycafe.dto.request.UserUpdateRequest;
 import store.studycafe.dto.response.UserResponse;
 
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -46,5 +43,17 @@ public class Usercontroller {
                 return new UserResponse(id, name, tel);
             }
         });
+    }
+
+    @PutMapping("/user")
+    public void updateUser(@RequestBody UserUpdateRequest request){
+        String sql = "UPDATE user SET tel = ? WHERE id = ?";
+        jdbcTemplate.update(sql, request.getTel(), request.getId());
+    }
+
+    @DeleteMapping("/user")
+    public void deleteUser(@RequestParam String tel){
+        String sql = "DELETE FROM user WHERE tel = ?";
+        jdbcTemplate.update(sql, tel);
     }
 }
